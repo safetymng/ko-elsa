6006bb22509749c1187ecec49b5ac37186039560
safetymng <160571050+safetymng@users.noreply.github.com>
2024-02-26 17:26:07 +0000

    test

--- common.js
+++ common.js
@@ -9,3 +9,8 @@ function login() {
     
 }
 var input = document.getElementById("pw");
+
+input.addEventListener("keyup", function (event) {
+    console.log(event.keyCode);
+    console.log(event.code);
+});
